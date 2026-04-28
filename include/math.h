ab91ebc44360fd37fa8093492f9e62859bbbb9d6
leonsuaudeau <friedrichsuaudeau@gmail.com>
2026-04-29 01:36:36 +0200

    Continued working on formulas

--- include/math.h
+++ include/math.h
@@ -3,65 +3,68 @@
 #include <vector>
 #include <glm/glm.hpp>
 #include <glm/vec2.hpp>
-#include <glm/gtx/norm.hpp>
 
 #include "particle.h"
 
-glm::vec2 inline euler_cromer_pos_step(const glm::vec2 x_t, const glm::vec2 v_t_next, const float h) {
-    return x_t + v_t_next * h;
+glm::vec2 inline euler_cromer_pos_step(const glm::vec2 x_t, const glm::vec2 v_t_next, const float dt) {
+    return x_t + v_t_next * dt;
 }
-glm::vec2 inline euler_cromer_vel_step(const glm::vec2 v_t, const glm::vec2 a_t, const float h) {
-    return v_t * a_t *  h;
+glm::vec2 inline euler_cromer_vel_step(const glm::vec2 v_t, const glm::vec2 a_t, const float dt) {
+    return v_t + a_t * dt;
 }
 
 float inline kernel_func_2D(const glm::vec2 x_i, const glm::vec2 x_j, const float h) {
-    const float q = glm::length(x_j - x_i) / h;
-    if (q >= 2) return 0;
+    const float q = glm::length(x_i - x_j) / h;
+    if (q >= 2.0f) return 0.0f;
 
     const float alpha = 5.0f / (14.0f * 3.14159265359f * h * h);
-    float w_q = 0;
-    float base = 2 - q;
-    w_q += base * base * base;
-    if (q < 1) {
-        base = 1 - q;
-        w_q -= 4 * base * base * base;
+    float w_q = (2.0f - q) * ((2.0f - q) * (2.0f - q));
+    if (q < 1.0f) {
+        w_q -= 4.0f * ((1.0f - q) * (1.0f - q) * (1.0f - q));
     }
     return alpha * w_q;
 }
 
 glm::vec2 inline kernel_deriv_2D(const glm::vec2 x_i, const glm::vec2 x_j, const float h) {
-    const float q = glm::length(x_j - x_i) / h;
-    if (q >= 2) return glm::vec3(0);
+    const float q = glm::length(x_i - x_j) / h;
+    if (q >= 2.0f) return glm::vec2(0.0f);
 
-    const glm::vec2 alpha = 5.0f * (x_j - x_i) / 14.0f * 3.14159265359f * h * h * h * glm::length(x_j - x_i);
-    auto w_q = glm::vec2(0);
-    float base = 2 - q;
-    w_q -= 3 * base * base;
-    if (q < 1) {
-        base = 1 - q;
-        w_q += 12 * base * base;
+    const float alpha = 5.0f / (14.0f * 3.14159265359f * h * h);
+    float w_q = -3.0f * ((2.0f - q) * (2.0f - q));
+    if (q < 1.0f) {
+        w_q += 12.0f * ((1.0f - q) * (1.0f - q));
     }
-    return alpha * w_q;
+    return alpha * w_q * ((x_i - x_j) / (glm::length(x_i - x_j) * h));
 }
 
-void inline update_density_explicit(Particle2D &p_i, const std::vector<Particle2D> &particles, const float h) {
+float inline density_explicit(const Particle2D &p_i, const std::vector<Particle2D> &neighbors, const float h) {
     float sum = 0;
-    for (const auto &p_j : particles) {
+    for (const auto &p_j : neighbors) {
         sum += p_j.mass * kernel_func_2D(p_i.pos, p_j.pos, h);
     }
-    p_i.density = sum;
+    return sum;
 }
 
-void inline update_pressure(Particle2D &p_i, const float k, const float rho_0) {
-    p_i.pressure = k * (p_i.density / rho_0) - 1;
+float inline pressure(const Particle2D &p_i, const float k, const float rho_0) {
+    return  glm::max(k * (p_i.density / rho_0) - 1, 0.0f);
 }
 
-glm::vec2 inline get_pressure_accel(const Particle2D &p_i, const std::vector<Particle2D> &particles, const float h) {
+glm::vec2 inline get_pressure_accel(const Particle2D &p_i, const std::vector<Particle2D> &neighbors, const float h) {
     auto sum = glm::vec2(0);
-    float p_i_press_by_density_squared = p_i.pressure / (p_i.density * p_i.density);
-    for (const auto &p_j : particles) {
-        sum += p_j.mass * (p_i_press_by_density_squared + p_j.pressure / (p_j.density * p_j.density) * kernel_deriv_2D(p_i.pos, p_j.pos, h));
+    const float i_frac = p_i.pressure / (p_i.density * p_i.density);
+    for (const auto &p_j : neighbors) {
+        sum += p_j.mass * (i_frac + p_j.pressure / (p_j.density * p_j.density)) * kernel_deriv_2D(p_i.pos, p_j.pos, h);
     }
+    return -sum;
+}
+
+glm::vec2 inline get_viscosity_accel(const Particle2D &p_i, const std::vector<Particle2D> &neighbors, const float h, const float nu) {
+    auto sum = glm::vec2(0);
+    for (const auto &p_j : neighbors) {
+        glm::vec2 x_ij = p_j.pos - p_i.pos;
+        sum += p_j.mass / p_j.density * ((p_j.vel - p_i.vel) * x_ij) / (x_ij * x_ij + 0.01f * h * h) * kernel_deriv_2D(p_i.pos, p_j.pos, h);
+    }
+    return 2 * nu * sum;
 }
 
 #endif //SPH_FLUID_SOLVER_MATH_H
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,46 @@
 #include <imgui_impl_opengl3.h>
 #include "particle.h"
 #include "camera.h"
+#include "math.h"
 #include "shader_utilities.h"
 
 static void glfw_error_callback(int error, const char* description) {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+inline void fluid_solver_iteration(std::vector<Particle2D> &particles) {
+    float h = 2;
+    float dt = 0.001f;
+    std::vector<std::vector<Particle2D>> neighborIndices{};
+    std::vector<glm::vec2> accelerations{};
+
+    for (int i = 0; i < particles.size(); i++) {
+        neighborIndices.emplace_back();
+        for (int j = 0; j < particles.size(); j++) {
+            glm::vec2 p_ij = particles[j].pos - particles[i].pos;
+            if (length(p_ij) <= 2 * h && i != j) {
+                neighborIndices[i].push_back(particles[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i].density = density_explicit(particles[i], neighborIndices[i], h);
+        particles[i].pressure = pressure(particles[i], 10, 1);
+    }
+
+    for (int i = 0; i < particles.size(); i++) {
+        accelerations.emplace_back(0, 0);
+        accelerations[i] -= glm::vec2(0, 9.81f);
+        accelerations[i] += get_viscosity_accel(particles[i], neighborIndices[i], h, 0.000001f);
+        accelerations[i] += get_pressure_accel(particles[i], neighborIndices[i], h);
+    }
+
+    for (int i = 0; i < particles.size(); i++) {
+        Particle2D &p_i = particles[i];
+        p_i.vel = euler_cromer_vel_step(p_i.vel, accelerations[i], dt);
+        p_i.pos = euler_cromer_pos_step(p_i.pos, p_i.vel, dt);
+    }
 }
 
 int main() {
@@ -21,7 +57,7 @@ int main() {
 
     for (int y = -8; y <= 8; y++) {
         for (int x = -10; x <= 10; x++) {
-            particles.emplace_back(Particle2D(glm::vec2(x, y), glm::vec2(0, 0), 1));
+            particles.emplace_back(Particle2D(glm::vec2(x, y), glm::vec2(0, 0), 1, 0, 1));
         }
     }
     std::cout << "Number of particles: " << particles.size() << std::endl;
@@ -74,6 +110,8 @@ int main() {
         if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) camera.move(0, -1);
         if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) camera.move(-1, 0);
         if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) camera.move(1, 0);
+
+        fluid_solver_iteration(particles);
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
